f9a3d541cb4d50873e99c14d4b4672a7bdda307d
nialljmiller <niall.j.miller@gmail.com>
2025-03-15 09:08:39 -0600

    Removed unnecessary files

--- hook-OpenGL.py
+++
@@ -1,6 +0,0 @@
-from PyInstaller.utils.hooks import collect_all
-
-# Collect all OpenGL related packages
-datas, binaries, hiddenimports = collect_all('OpenGL')
-datas += collect_all('OpenGL.platform')[0]
-datas += collect_all('OpenGL.arrays')[0]
